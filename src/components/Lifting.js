502124542f43db8bff6f7e2621833b93cc8c3c4e
Pranav Sathianathan <pranav@stevenson-100-64-40-121.resnet.ucsc.edu>
2024-02-11 23:59:13 -0800

    working on lifting functionality, edited content inside it

--- src/components/Lifting.js
+++ src/components/Lifting.js
@@ -1,30 +1,39 @@
-import '../styles/Lifting.css'
+import React, { useState } from 'react';
+import '../styles/Lifting.css';
 import TextElement from './TextElement';
 import CompoundLift from './CompoundLift';
 
-function Lifting(){
-    const compoundLiftTypes = ['Squat', 'Bench', 'Deadlift']
+function Lifting() {
+    const compoundLiftTypes = ['Squat', 'Bench', 'Deadlift'];
+    const [liftActive, setLiftActive] = useState(false);
+
+    const handleLiftToggle = (isActive) => {
+        setLiftActive(isActive);
+    };
+
     const listLifts = compoundLiftTypes.map((compoundLiftType, index) => (
-  <CompoundLift
-    key={compoundLiftType}
-    type={compoundLiftType}
-    style={{
-      borderRadius: index === 0 ? '3vw 1vw 1vw 0' : 
-      index === compoundLiftTypes.length - 1 ? '0 1vw 1vw 3vw' :   // Last: no top left radius
-                    '0 1vw 1vw 0'  // Middle: no top left or bottom left radius
-    }}
-  />
-));
-    return(
+        <CompoundLift
+            key={compoundLiftType}
+            type={compoundLiftType}
+            style={{
+                borderRadius: index === 0 ? '3vw 1vw 1vw 0' :
+                    index === compoundLiftTypes.length - 1 ? '0 1vw 1vw 3vw' :
+                        '0 1vw 1vw 0'
+            }}
+            onToggle={handleLiftToggle}
+        />
+    ));
+
+    return (
         <div className='lifting-container'>
             <div>
-            {listLifts}
+                {listLifts}
             </div>
-            <div>
-               <TextElement
-            title = 'Weightlifting 🏋🏽'
-            bodyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Nisi lacus sed viverra tellus in hac habitasse. Nibh tellus molestie nunc non blandit massa enim. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus."
-            /> 
+            <div className={`text-element-container${liftActive ? 'hidden' : ''}`}>
+                <TextElement
+                    title='Weightlifting 🏋🏽'
+                    bodyText="I started working out in 2023, and so far I've made some great progress. However, I still have a long way to go if I ever want to compete in powerlifting. There have been up and downs. Nonetheless, we stay persistent! Here, I'll keep my record of the Big Three Lifts. I'll also show what I lifted when I first started:)."
+                />
             </div>
         </div>
     );
